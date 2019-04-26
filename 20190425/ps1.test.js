e6c0eed9465747d79c879ad6acf0505c32c9b5dd
wholemann <wholeman.dev@gmail.com>
2019-04-26 18:42:38 +0900

    Implement int2Str method

--- 20190425/ps1.test.js
+++ 20190425/ps1.test.js
@@ -5,7 +5,13 @@
 */
 
 function int2Str(input) {
-  return '123';
+  let numbers = [1, 2, 3];
+  let result = '';
+  for(let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+  }
+  
+  return result;
 }
 
 test('int2Str', () => {
