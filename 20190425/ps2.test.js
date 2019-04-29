8898ea8393fe71157a7cc588741cf9adf14d64b4
wholemann <wholeman.dev@gmail.com>
2019-04-29 22:38:40 +0900

    Initial Commit problem2

---
+++ 20190425/ps2.test.js
@@ -0,0 +1,14 @@
+/*
+1. 문자열의 배열로 생각하고 문자를 한개씩 읽는다.
+2. 공백을 읽었을 경우 공백 이전 인덱스까지 문자열을 배열에 첫번째 요소로 넣는다.
+3. 2번을 반복한다.
+4. 마지막 인덱스에 도달하면 이 과정을 종료한다.
+*/
+
+function reverseString(inputStr) {
+  return 'boy. a am I';
+}
+
+test('reverseString', () => {
+  expect(reverseString('I am a boy.')).toBe('boy. a am I');
+});
