ef86d4a77a231a7c240e63f133a0aba77cb40222
wholemann <wholeman.dev@gmail.com>
2019-04-29 18:22:06 +0900

    Refactor divideNumberOfdigits method

--- 20190425/ps1.test.js
+++ 20190425/ps1.test.js
@@ -4,25 +4,32 @@
 3. 배열의 각 요소를 문자열 더하기를 통해 하나의 문자열로 만든다. 이를 위한 도구가 있다면 찾아보고 이용한다.
 */
 
-
 function divideNumberOfDigits(input) {
   const numbers = [];
-  const firstDigit = input % 10;
-  numbers.unshift(firstDigit);
-  console.log(firstDigit);
-  const secondDigit = ((input - firstDigit) / 10) % 10;
-  numbers.unshift(secondDigit);
-  console.log(secondDigit);
-  const thirdDigit = ((input - (secondDigit * 10 + firstDigit * 1)) / 100) % 10;
-  numbers.unshift(thirdDigit);
-  console.log(thirdDigit);
-  
+
+  if (input == 0) {
+    numbers.push(input);
+    return numbers;
+  }
+
+  let digit;
+  while (input != 0) {
+    digit = input % 10;
+    numbers.unshift(digit);
+    input = (input - digit) / 10;
+  }
+
   return numbers;
 }
 
 function int2Str(input) {
-  let numbers = divideNumberOfDigits(input);
   let result = '';
+  if (input < 0) {
+    result = '-';
+    input *= -1;
+  }
+  let numbers = divideNumberOfDigits(input);
+  
   for(let i = 0; i < numbers.length; i++) {
     result += numbers[i];
   }
@@ -32,7 +39,34 @@ function int2Str(input) {
 test('int2Str', () => {
   expect(int2Str(123)).toBe('123');
 });
+test('int2Str', () => {
+  expect(int2Str(130)).toBe('130');
+});
+test('int2Str', () => {
+  expect(int2Str(13)).toBe('13');
+});
+test('int2Str', () => {
+  expect(int2Str(0)).toBe('0');
+});
+test('int2Str', () => {
+  expect(int2Str(-123)).toBe('-123');
+});
 
 test('divideNumberOfDigits', () => {
   expect(divideNumberOfDigits(123)).toEqual([1, 2, 3]);
 });
+test('divideNumberOfDigits', () => {
+  expect(divideNumberOfDigits(103)).toEqual([1, 0, 3]);
+});
+test('divideNumberOfDigits', () => {
+  expect(divideNumberOfDigits(130)).toEqual([1, 3, 0]);
+});
+test('divideNumberOfDigits', () => {
+  expect(divideNumberOfDigits(1)).toEqual([1]);
+});
+test('divideNumberOfDigits', () => {
+  expect(divideNumberOfDigits(0)).toEqual([0]);
+});
+// test('divideNumberOfDigits', () => {
+//   expect(divideNumberOfDigits(-123)).toEqual([-1, 2, 3]);
+// });
