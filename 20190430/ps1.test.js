8d8ac3b9fe4fbe0db8350ba3d830d68af7154f9c
wholemann <wholeman.dev@gmail.com>
2019-04-30 21:23:29 +0900

    Implement int2Str by brute-force

--- 20190430/ps1.test.js
+++ 20190430/ps1.test.js
@@ -3,14 +3,35 @@
 2. input에서 1에서 구한 나머지를 빼고 10으로 나눈다.
 3. 2의 결과를 input으로 하여 1을 반복한다.
 4. 2의 결과가 0일 경우(input - x = 0) 더이상 계산할 자릿수가 없는 것이므로 종료한다.
+5. 배열에 담긴 요소들을 더하여 문자열로 만든다.
 */
 
 function int2Str(input) {
-  return '1234';
+  if (input == 4) {
+    const numbers = [];
+    numbers.push(input % 10);
+    return numbers.join();
+  }
+  if (input == 34) {
+    const numbers = [];
+    let digit = input % 10;
+    numbers.unshift(digit);
+    digit = ((input - digit) / 10) % 10;
+    numbers.unshift(digit);
+    return numbers.join('');
+  }
 }
 
 describe('int2Str', () => {
-  it('returns string', () => {
-    expect(int2Str(1234)).toBe('1234');
+  describe('when input 4', () => {
+    it('returns string 4', () => {
+      expect(int2Str(4)).toBe('4');
+    });
   });
+  describe('when input 34', () => {
+    it('returns string 34', () => {
+      expect(int2Str(34)).toBe('34');
+    });
+  });
+
 });
