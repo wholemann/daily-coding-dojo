a5aa45ca076849941bdff3d1b88b770b9fd78020
wholemann <wholeman.dev@gmail.com>
2019-05-02 17:17:41 +0900

    Initial Commit two_stack_to_queue

--- 20190502/two_stack_to_queue.test.js
+++ 20190502/two_stack_to_queue.test.js
@@ -1,5 +1,19 @@
 test('queue', () => {
 
+  const queue = {
+    
+  };
 
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+
+  expect(queue.dequeue()).toBe(1);
+  expect(queue.dequeue()).toBe(2);
+
+  queue.enqueue(4);
+  
+  expect(queue.dequeue()).toBe(3);
+  expect(queue.dequeue()).toBe(4);
 
 });
