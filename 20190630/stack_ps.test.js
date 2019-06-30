5e227cc6d4afed2ffc3170bf2e709d0b97290313
wholemann <wholeman.dev@gmail.com>
2019-07-01 00:21:02 +0900

    Modify space in code

--- 20190630/stack_ps.test.js
+++ 20190630/stack_ps.test.js
@@ -14,6 +14,6 @@ function getReceiveOrder(heights) {
 
 test('getReceiveOrder', () => {
   expect(getReceiveOrder([6, 9, 5, 7, 4])).toEqual([0, 0, 2, 2, 4]);
-  expect(getReceiveOrder([3, 9, 9, 3, 5, 7, 2])).toEqual(	[0, 0, 0, 3, 3, 3, 6]);
+  expect(getReceiveOrder([3, 9, 9, 3, 5, 7, 2])).toEqual([0, 0, 0, 3, 3, 3, 6]);
   expect(getReceiveOrder([1, 5, 3, 6, 7, 6, 5])).toEqual([0, 0, 2, 0, 0, 5, 6]);
 });
