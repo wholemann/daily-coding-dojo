497760c2aef400d9035396f9fdf8f5600b259fdd
wholemann <wholeman.dev@gmail.com>
2019-07-02 02:37:22 +0900

    Refactor sender receiver problem

--- 20190630/stack_ps.test.js
+++ 20190630/stack_ps.test.js
@@ -1,3 +1,7 @@
+function getNearestReceiver(stack, sender) {
+  return stack.map(v => v > sender ? true : false).lastIndexOf(true) + 1;
+}
+
 function getReceiveOrder(heights) {
 
   const stack = [...heights];
@@ -5,8 +9,7 @@ function getReceiveOrder(heights) {
   
   while (!(stack.length === 0)) {
     const sender = stack.pop();
-    result.unshift(
-      stack.map(v => v > sender ? true : false).lastIndexOf(true) + 1);
+    result.unshift(getNearestReceiver(stack, sender));
   }
 
   return result;
@@ -16,4 +19,9 @@ test('getReceiveOrder', () => {
   expect(getReceiveOrder([6, 9, 5, 7, 4])).toEqual([0, 0, 2, 2, 4]);
   expect(getReceiveOrder([3, 9, 9, 3, 5, 7, 2])).toEqual([0, 0, 0, 3, 3, 3, 6]);
   expect(getReceiveOrder([1, 5, 3, 6, 7, 6, 5])).toEqual([0, 0, 2, 0, 0, 5, 6]);
+});
+
+test('getNearestReceiver', () => {
+  expect(getNearestReceiver([6, 9, 5, 7], 4)).toBe(4);
+  expect(getNearestReceiver([3, 9, 9, 3, 5, 7], 2)).toBe(6);
 });
