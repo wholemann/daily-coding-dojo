81c4586923e3fc6579d52d1ef7f9681f389980b3
wholemann <wholeman.dev@gmail.com>
2019-12-23 15:04:13 +0900

    Solve strange string problem by Kotlin
    See also
        - https://programmers.co.kr/learn/courses/30/lessons/12930

--- 20191223/kotlin/src/test/kotlin/com/wholeman/strangestring/SolutionTest.kt
+++ 20191223/kotlin/src/test/kotlin/com/wholeman/strangestring/SolutionTest.kt
@@ -1,4 +1,21 @@
 package com.wholeman.strangestring
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+fun createStrangeString(s: String) =
+        s.split(" ").joinToString(" ") { word ->
+            word
+                    .mapIndexed { index, char ->
+                        if (index % 2 == 0) char.toUpperCase() else char.toLowerCase()
+                    }
+                    .joinToString("")
+        }
+
 class SolutionTest {
+
+    @Test
+    fun `Make a strange string by following the rules`() {
+        assertThat(createStrangeString("try hello world")).isEqualTo("TrY HeLlO WoRlD")
+    }
 }
