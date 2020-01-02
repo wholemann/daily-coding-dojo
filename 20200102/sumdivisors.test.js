4a0b8a085c3338045591681bef878b015aa8cbe1
wholemann <wholeman.dev@gmail.com>
2020-01-02 23:59:36 +0900

    Add missing test case about 0
    In the previous code, the test fails when input is 0.
    Because Array(0) cannot create an array.

--- 20200102/sumdivisors.test.js
+++ 20200102/sumdivisors.test.js
@@ -1,11 +1,14 @@
 const solution = (n) => 
-  Array(n)
-    .fill()
-    .map((_, i) => i + 1)
-    .filter(x => n % x === 0)
-    .reduce((a, b) => a + b);
+  n !== 0 
+    ? Array(n)
+      .fill()
+      .map((_, i) => i + 1)
+      .filter(x => n % x === 0)
+      .reduce((a, b) => a + b)
+    : n
 
 test('solution', () => {
   expect(solution(12)).toBe(28);
   expect(solution(5)).toBe(6);
+  expect(solution(0)).toBe(0);
 });
