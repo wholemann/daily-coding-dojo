5820d75068807e24eab431f11bdd18f7b706b250
wholemann <wholeman.dev@gmail.com>
2020-01-03 17:23:42 +0900

    Modify test case expression

--- 20200103/square.test.js
+++ 20200103/square.test.js
@@ -2,7 +2,7 @@ const solution = (n) =>
   Number.isInteger(n ** (1 / 2)) ? (n ** (1 / 2) + 1) ** 2 : -1;
 
 test('solution', () => {
-  expect(solution(1000000)).toBe(1001 * 1001);
-  expect(solution(121)).toBe(144);
+  expect(solution(1000000)).toBe((1000 + 1) ** 2);
+  expect(solution(121)).toBe((11 + 1) ** 2);
   expect(solution(3)).toBe(-1);
 });
