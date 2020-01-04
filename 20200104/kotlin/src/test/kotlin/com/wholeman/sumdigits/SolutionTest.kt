a9c61613e8095fd4f0a7cef57f0e50d9acd59965
wholemann <wholeman.dev@gmail.com>
2020-01-04 23:36:54 +0900

    Solve sum of digits problem by Kotlin
    See also
        - https://programmers.co.kr/learn/courses/30/lessons/12931

--- 20200104/kotlin/src/test/kotlin/com/wholeman/sumdigits/SolutionTest.kt
+++ 20200104/kotlin/src/test/kotlin/com/wholeman/sumdigits/SolutionTest.kt
@@ -3,12 +3,14 @@ package com.wholeman.sumdigits
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-
+fun solution(n: Int) = "$n".map(Character::getNumericValue).sum()
 
 class SolutionTest {
 
     @Test
     fun `Find the sum of each digit in a given natural number`() {
-        assertThat(1 + 1).isEqualTo(2)
+        assertThat(solution(123)).isEqualTo(6)
+        assertThat(solution(987)).isEqualTo(24)
+        assertThat(solution(1)).isEqualTo(1)
     }
 }
