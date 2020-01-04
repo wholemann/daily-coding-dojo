956565248153f03a6e4b76c8fa94bc32a2199f0d
wholemann <wholeman.dev@gmail.com>
2020-01-04 23:04:54 +0900

    Initial commit for Kotlin

---
+++ 20200104/kotlin/src/test/kotlin/com/wholeman/sumdigits/SolutionTest.kt
@@ -0,0 +1,14 @@
+package com.wholeman.sumdigits
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+
+
+class SolutionTest {
+
+    @Test
+    fun `Find the sum of each digit in a given natural number`() {
+        assertThat(1 + 1).isEqualTo(2)
+    }
+}
