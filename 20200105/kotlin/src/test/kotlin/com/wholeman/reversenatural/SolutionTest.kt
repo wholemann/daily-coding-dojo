0e94f94bca56bd4f747d4c1f86a1a9d08e15c076
wholemann <wholeman.dev@gmail.com>
2020-01-05 23:45:11 +0900

    Solve reverse natural number problem by Kotlin

--- 20200105/kotlin/src/test/kotlin/com/wholeman/reversenatural/SolutionTest.kt
+++ 20200105/kotlin/src/test/kotlin/com/wholeman/reversenatural/SolutionTest.kt
@@ -1,5 +1,15 @@
 package com.wholeman.reversenatural
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+fun solution(n: Long) = "$n".map(Character::getNumericValue).reversed().toIntArray()
+
 class SolutionTest {
 
+    @Test
+    fun `Convert natural number to Invert a given natural number to form an array`() {
+        assertThat(solution(12345)).isEqualTo(intArrayOf(5, 4, 3, 2, 1))
+        assertThat(solution(1)).isEqualTo(intArrayOf(1))
+    }
 }
