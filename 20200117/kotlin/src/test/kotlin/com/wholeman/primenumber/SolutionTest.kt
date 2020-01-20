9559318bee3c99796e077e52d7a8d6065101cde5
wholemann <wholeman.dev@gmail.com>
2020-01-20 15:49:03 +0900

    Solve prime number count problem by Kotlin

--- 20200117/kotlin/src/test/kotlin/com/wholeman/primenumber/SolutionTest.kt
+++ 20200117/kotlin/src/test/kotlin/com/wholeman/primenumber/SolutionTest.kt
@@ -3,16 +3,37 @@ package com.wholeman.primenumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-fun solution(nums: IntArray): Int {
-    return 1
+fun solution(nums: IntArray): Int = sumOfThree2(nums).count { it.isPrime() }
+
+fun Int.isPrime(): Boolean = primes(kotlin.math.sqrt(this.toDouble()).toInt()).none { this % it == 0 }
+
+fun primes(n: Int): Set<Int> = (2..n).filter { it.isPrime() }.toSet()
+
+// for kotlin old version
+fun sumOfThree1(nums: IntArray): List<Int> {
+    val result = mutableListOf<Int>()
+    (0 until nums.size - 2).forEach { i ->
+        (i + 1 until nums.size - 1).forEach { j ->
+            (j + 1 until nums.size).forEach { k ->
+                result.add(nums[i] + nums[j] + nums[k])
+            }
+        }
+    }
+    return result
 }
 
-fun Int.isPrime(): Boolean {
-    return this in setOf(2, 3, 5, 7)
-}
+fun sumOfThree2(nums: IntArray): List<Int> = sequence {
+    (0 until nums.size - 2).forEach { i ->
+        (i + 1 until nums.size - 1).forEach { j ->
+            (j + 1 until nums.size).forEach { k ->
+                yield(nums[i] + nums[j] + nums[k])
+            }
+        }
+    }
+}.toList()
 
-fun <T> Collection<T>.combinations(nums: IntArray, k: Int) = when {
-    else -> powerSet(nums.toSet(), setOf(emptySet())).filter { it.size == k }
+fun <T> Set<T>.combinations(k: Int) = when {
+    else -> powerSet(this, setOf(emptySet())).filter { it.size == k }
 }
 
 tailrec fun <T> powerSet(left: Collection<T>, acc: Set<Set<T>>): Set<Set<T>> = when {
@@ -25,20 +46,32 @@ class SolutionTest {
     @Test
     fun `Count the number of prime numbers made by adding three numbers`() {
         assertThat(solution(intArrayOf(1, 2, 3, 4))).isEqualTo(1)
+        assertThat(solution(intArrayOf(1, 2, 7, 6, 4))).isEqualTo(4)
     }
 
     @Test
     fun `Determine whether the given number is prime`() {
         assertThat(2.isPrime()).isEqualTo(true)
-//        assertThat(isPrime(3)).isEqualTo(true)
-//        assertThat(isPrime(4)).isEqualTo(false)
+        assertThat(3.isPrime()).isEqualTo(true)
+        assertThat(4.isPrime()).isEqualTo(false)
+        assertThat(11.isPrime()).isEqualTo(true)
+    }
+
+    @Test
+    fun `Create the set of prime numbers in range 2 to n`() {
+        assertThat(primes(12)).isEqualTo(setOf(2, 3, 5, 7, 11))
+    }
+
+    @Test
+    fun `Create the set of numbers made by sum of three numbers`() {
+        assertThat(sumOfThree(intArrayOf(1, 2, 3, 4))).isEqualTo(listOf(6, 7, 8, 9))
     }
 
     @Test
     fun `Calculate the set of combinations about given Collection`() {
-//        assertThat(combinations(intArrayOf(1, 2, 3), 2)).isEqualTo(
-//                listOf(setOf(1, 2), setOf(1, 3), setOf(2, 3))
-//        )
+        assertThat(setOf(1, 2, 3).combinations(2)).isEqualTo(
+                listOf(setOf(1, 2), setOf(1, 3), setOf(2, 3))
+        )
     }
 
     @Test
