cea64845c2aaaa0100a8fbf0af21f5b6e667c790
wholemann <wholeman.dev@gmail.com>
2020-01-21 10:10:29 +0900

    Remove useless code
    Transfer combination, powerset to another code base.

--- 20200117/kotlin/src/test/kotlin/com/wholeman/primenumber/SolutionTest.kt
+++ 20200117/kotlin/src/test/kotlin/com/wholeman/primenumber/SolutionTest.kt
@@ -2,27 +2,15 @@ package com.wholeman.primenumber
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import kotlin.math.sqrt
 
-fun solution(nums: IntArray): Int = sumOfThree2(nums).count { it.isPrime() }
+fun solution(nums: IntArray): Int = sumOfThree(nums).count { it.isPrime() }
 
-fun Int.isPrime(): Boolean = primes(kotlin.math.sqrt(this.toDouble()).toInt()).none { this % it == 0 }
+fun Int.isPrime(): Boolean = primes(sqrt(this.toDouble()).toInt()).none { this % it == 0 }
 
 fun primes(n: Int): Set<Int> = (2..n).filter { it.isPrime() }.toSet()
 
-// for kotlin old version
-fun sumOfThree1(nums: IntArray): List<Int> {
-    val result = mutableListOf<Int>()
-    (0 until nums.size - 2).forEach { i ->
-        (i + 1 until nums.size - 1).forEach { j ->
-            (j + 1 until nums.size).forEach { k ->
-                result.add(nums[i] + nums[j] + nums[k])
-            }
-        }
-    }
-    return result
-}
-
-fun sumOfThree2(nums: IntArray): List<Int> = sequence {
+fun sumOfThree(nums: IntArray): List<Int> = sequence {
     (0 until nums.size - 2).forEach { i ->
         (i + 1 until nums.size - 1).forEach { j ->
             (j + 1 until nums.size).forEach { k ->
@@ -32,13 +20,17 @@ fun sumOfThree2(nums: IntArray): List<Int> = sequence {
     }
 }.toList()
 
-fun <T> Set<T>.combinations(k: Int) = when {
-    else -> powerSet(this, setOf(emptySet())).filter { it.size == k }
-}
-
-tailrec fun <T> powerSet(left: Collection<T>, acc: Set<Set<T>>): Set<Set<T>> = when {
-    left.isEmpty() -> acc
-    else -> powerSet(left.drop(1), acc + acc.map { it + left.first() })
+// for kotlin old version
+fun sumOfThreeOld(nums: IntArray): List<Int> {
+    val result = mutableListOf<Int>()
+    (0 until nums.size - 2).forEach { i ->
+        (i + 1 until nums.size - 1).forEach { j ->
+            (j + 1 until nums.size).forEach { k ->
+                result.add(nums[i] + nums[j] + nums[k])
+            }
+        }
+    }
+    return result
 }
 
 class SolutionTest {
@@ -64,21 +56,8 @@ class SolutionTest {
 
     @Test
     fun `Create the set of numbers made by sum of three numbers`() {
-        arrayOf(::sumOfThree1, ::sumOfThree2).forEach { sumOfThree ->
+        arrayOf(::sumOfThree, ::sumOfThreeOld).forEach { sumOfThree ->
             assertThat(sumOfThree(intArrayOf(1, 2, 3, 4))).isEqualTo(listOf(6, 7, 8, 9))
         }
-    }
-
-    @Test
-    fun `Calculate the set of combinations about given Collection`() {
-        assertThat(setOf(1, 2, 3).combinations(2)).isEqualTo(
-                listOf(setOf(1, 2), setOf(1, 3), setOf(2, 3))
-        )
-    }
-
-    @Test
-    fun `Create the powerSet`() {
-        assertThat(powerSet(setOf(1, 2), setOf(emptySet())))
-                .isEqualTo(setOf(emptySet(), setOf(1), setOf(2), setOf(1, 2)))
     }
 }
