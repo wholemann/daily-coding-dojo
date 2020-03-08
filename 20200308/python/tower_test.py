0e07b066363ca08e3dadcb59ebd3c4f8c6f16b06
wholemann <wholeman.dev@gmail.com>
2020-03-08 17:45:06 +0900

    Solve towers problem by Python

---
+++ 20200308/python/tower_test.py
@@ -0,0 +1,19 @@
+# 1. 송신탑보다 큰 수신탑 중에 가장 index가 큰 탑을 찾는다.
+# 2. 수신탑의 index + 1를 리스트 앞쪽에 넣는다.
+# 3. 수신탑이 없으면 0을 넣는다.
+
+
+def solution(heights):
+    receivers = []
+    while heights:
+        s, heights = heights[-1], heights[:-1]
+        indexes = [i for i, r in enumerate(heights) if s < r]
+        receivers.insert(0, indexes and max(indexes) + 1 or 0)
+
+    return receivers
+
+
+def test_solution():
+    assert solution([6, 9, 5, 7, 4]) == [0, 0, 2, 2, 4]
+    assert solution([3, 9, 9, 3, 5, 7, 2]) == [0, 0, 0, 3, 3, 3, 6]
+    assert solution([1, 5, 3, 6, 7, 6, 5]) == [0, 0, 2, 0, 0, 5, 6]
