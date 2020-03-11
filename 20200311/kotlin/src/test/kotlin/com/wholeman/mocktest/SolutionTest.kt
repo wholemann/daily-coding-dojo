121291879ed55371379cf8567f2d624f1036c3d2
wholemann <wholeman.dev@gmail.com>
2020-03-11 23:37:37 +0900

    Solve mock test problem by Kotlin

--- 20200311/kotlin/src/test/kotlin/com/wholeman/mocktest/SolutionTest.kt
+++ 20200311/kotlin/src/test/kotlin/com/wholeman/mocktest/SolutionTest.kt
@@ -9,14 +9,15 @@ fun solution(answers: IntArray): IntArray {
             listOf(2, 1, 2, 3, 2, 4, 2, 5),
             listOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
     )
+    val scores = intArrayOf(0, 0, 0)
 
-    val scores = answers.foldIndexed(Triple(0, 0, 0)) { index, acc, c ->
-        when (c) {
-            patterns[0][index % patterns[0].size] -> Triple(acc.first + 1, acc.second, acc.third)
-            patterns[1][index % patterns[1].size] -> Triple(acc.first, acc.second + 1, acc.third)
-            else -> Triple(acc.first, acc.second, acc.third + 1)
+    answers.forEachIndexed { index, i ->
+        (0..2).forEach { j ->
+            if (patterns[j][index % answers.size] == i) {
+                scores[j] += 1
+            }
         }
-    }.toList()
+    }
 
     return scores.foldIndexed(intArrayOf()) { index, acc, s ->
         when (s) {
@@ -31,5 +32,6 @@ class SolutionTest {
     @Test
     fun `Find the person who solved the most problems`() {
         assertThat(solution(intArrayOf(1, 2, 3, 4, 5))).isEqualTo(intArrayOf(1))
+        assertThat(solution(intArrayOf(1, 3, 2, 4, 2))).isEqualTo(intArrayOf(1, 2, 3))
     }
 }
